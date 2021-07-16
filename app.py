11ab82c84271d88707e5891511ccde1381ac0966
Terrell Bradford <terrell.bradford@gmail.com>
2021-07-16 17:42:31 -0400

    Analysis Complete

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end>"
     )
 
@@ -50,6 +51,7 @@ def home():
 def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
+
     # Query
     last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
 
@@ -71,6 +73,7 @@ def precipitation():
 def stations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
+
     # Query
     station_activity = (session.query(Measurement.station, func.count(Measurement.station))
         .group_by(Measurement.station)
@@ -89,6 +92,7 @@ def stations():
 def tobs():
     # Create our session (link) from Python to the DB
     session = Session(engine)
+
     # Query
     lowest_temp = session.query(func.min(Measurement.tobs)).\
     filter(Measurement.station == "USC00519281").all()
@@ -112,29 +116,55 @@ def tobs():
 
 
 @app.route("/api/v1.0/<start>")
-@app.route("/api/v1.0/<start>/<end>")
 
-def start_end(start,end = None):
-# Create our session (link) from Python to the DB
+
+def start_end(start):
+
+#    # Create our session (link) from Python to the DB
     session = Session(engine)
-# Query
-    #start_date = dt.datetime.strptime(start, '%Y-%m-%d')
-    #end_date = dt.datetime.strptime(end, "%Y-%m-%d")
-    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs),func.max(Measurement.tobs)]
 
-    if not end:
-        #start1 = session.query(*sel).filter(dt.datetime.strftime("%m-%d", Measurement.date) == start_date).all() 
-        start1 = session.query(*sel).filter(Measurement.date >= start).all
-        start1 = list(np.ravel(start1))
-        return jsonify(start1)
+ 
+    # Query
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
 
-    #start2 = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
-    start2 = session.query(*sel).filter(Measurement.date >= start).filter(Measurement.date <=end).all
+    results = session.query(*sel).filter(Measurement.date >= start).all()
+
     session.close()
-    start2 = list(np.ravel(start2))
-    return jsonify(start2)
+
+    # Append data to empty dictionary then append dictionary to empty list
+    start_date = []
+    for min, avg, max in results:
+        start_date_dict = {}
+        start_date_dict[f"The Minimum Temperature on {start} was"] = min
+        start_date_dict[f"The Average Temperature on {start} was"] = avg
+        start_date_dict[f"The Maximum Temperature on {start} was"] = max
+        start_date.append(start_date_dict) 
+
+    return jsonify(start_date)
 
 
+@app.route("/api/v1.0/<start>/<end>")
+def Start_end_date(start, end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query 
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    results = session.query(*sel).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+    session.close()
+  
+    # Append data to empty dictionary then append dictionary to empty list
+    end_date = []
+    for min, avg, max in results:
+        end_date_dict = {}
+        end_date_dict[f"The Minimum Temperature from {start} through {end} was"] = min
+        end_date_dict[f"The Average Temperature from {start} through {end} was"] = avg
+        end_date_dict[f"The Maximum Temperature from {start} through {end} was"] = max
+        end_date.append(end_date_dict) 
+    
+    return jsonify(end_date)
 
 if __name__ == '__main__':
     app.run(debug=True)
